1cf669b73167f818bea1c76ed4cd670508f23fe0
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-08-26 15:51:59 +0900

    editoページを作成

---
+++ pages/edit.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import MarkdownEditor from "../components/markdown/MarkdownEditor";
+
+const EditPage = () => {
+  const [md, setMd] = useState("");
+  return (
+    <div>
+      <MarkdownEditor value={md} onChange={(e)=>{setMd(e)}} />
+    </div>
+  )
+}
+
+export default EditPage;
