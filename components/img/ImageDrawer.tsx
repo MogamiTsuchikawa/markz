1b534df07c4f82a9885a1cc638059d7e5c855758
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-08-28 00:41:44 +0900

    canvasに各部分を実装

--- components/img/ImageDrawer.tsx
+++ components/img/ImageDrawer.tsx
@@ -1,5 +1,63 @@
+import { useRef, useEffect, MouseEventHandler } from "react";
+
 const ImageDrawer = () => {
-  return <></>
-}
+  return (
+    <>
+      <DrawCanvas />
+    </>
+  );
+};
 
 export default ImageDrawer;
+
+const DrawCanvas = () => {
+  const canvasRef = useRef(null);
+  const getContext = (): CanvasRenderingContext2D => {
+    const canvas: any = canvasRef.current;
+    return canvas.getContext("2d");
+  };
+  useEffect(() => {
+    const ctx: CanvasRenderingContext2D = getContext();
+    ctx.save();
+  });
+  let lastPos = [0, 0];
+  let isDrawing = false;
+  const getPos = (e: any) => {
+    if (!canvasRef.current) return [0, 0];
+    var x, y;
+    const canvas: any = canvasRef.current;
+    x = e.clientX - canvas.getBoundingClientRect().left;
+    y = e.clientY - canvas.getBoundingClientRect().top;
+    return [x, y];
+  };
+  const onMouseDown: MouseEventHandler<HTMLCanvasElement> = (e) => {
+    if (!canvasRef.current) return;
+    isDrawing = true;
+    lastPos = getPos(e);
+  };
+  const onMouseMove: MouseEventHandler<HTMLCanvasElement> = (e) => {
+    if (!isDrawing) return;
+    const ctx = getContext();
+    ctx.moveTo(lastPos[0], lastPos[1]);
+    const pos = getPos(e);
+    ctx.lineTo(pos[0], pos[1]);
+    ctx.strokeStyle = "green";
+    ctx.stroke();
+    lastPos = pos;
+  };
+  const onMouseUp: MouseEventHandler<HTMLCanvasElement> = (e) => {
+    isDrawing = false;
+  };
+  return (
+    <>
+      <canvas
+        ref={canvasRef}
+        width={400}
+        height={400}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+      ></canvas>
+    </>
+  );
+};
--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
+import ImageDrawer from "../components/img/ImageDrawer";
 import MarkdownEditor from "../components/markdown/MarkdownEditor";
 
 const EditPage = () => {
   const [md, setMd] = useState("");
   return (
     <div>
-      <MarkdownEditor value={md} onChange={(e)=>{setMd(e)}} />
+      <MarkdownEditor
+        value={md}
+        onChange={(e) => {
+          setMd(e);
+        }}
+      />
+      <ImageDrawer />
     </div>
-  )
-}
+  );
+};
 
 export default EditPage;
