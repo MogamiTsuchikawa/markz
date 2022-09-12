2aa89477d7eabcd705b1a5f420b589c164abb0c8
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-09-12 12:38:58 +0900

    Markdownの表示を修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { CssBaseline } from "@mui/material";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <CssBaseline />
       <Component {...pageProps} />
     </RecoilRoot>
   );
